24310795b0c7e3fcdbe68b51f68d3efcdc7be76d
Dhairya Gugale <dhairyagugale7@gmail.com>
2026-02-20 12:17:34 +0000

    done

---
+++ 01_basics/03_normalOperations.js
@@ -0,0 +1,27 @@
+let str1 = "Dhairya"
+let str2 = " Manoj"
+let str3 = " Gugale"
+
+let name = str1 + str2 + str3
+
+console.log("Name : ",name);
+
+console.log(7 + 7 + " Dhairya " + "Gugale " + 7 + 7);
+
+console.log("Dhairya " + 7 + 7);   
+
+console.log(true + 2);  //true = 1 similarly false = 0
+
+console.log(null + 7);  //null = 0
+
+console.log(undefined + 7)
+
+let a = 100
+let b = ++a
+console.log(a); //101
+console.log(b); //101
+
+let c = 100
+let d = c++
+console.log(c); //101
+console.log(d); //100
